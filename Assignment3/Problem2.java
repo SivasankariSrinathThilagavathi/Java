24e8ab1831de06b33fee9e16e83e5c8dbec44a54
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-05-29 23:28:55 +0530

    Create Problem2.java

---
+++ Assignment3/Problem2.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+//Assignment3
+//Sivasankari Srinath Thilagavathi
+
+class Assignment3{
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+
+        //2.    WAP to find the sum of all the digits of a given number.
+        int num;
+        System.out.print("Enter the number: ");
+        num=ob.nextInt();
+        int sum=0;
+        while (num>0){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        System.out.println("The sum of digits is: "+sum);        
+
+    }
+}
