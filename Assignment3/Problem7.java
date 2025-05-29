19bb1d175de0f4bf1d57e0eaa3c5e5d5dca938fa
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-05-29 23:33:51 +0530

    Create Problem7.java

---
+++ Assignment3/Problem7.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+//Assignment3
+//Sivasankari Srinath Thilagavathi
+
+class Problem7{
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+
+        //7.
+        for(int i=1;i<=100;i=i+2){
+            System.out.print(i+"\t");
+        }
+        
+    }
+}
