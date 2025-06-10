3ed6fda8f10c87fa97ec9855c795a8b9c1ab653a
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:22:20 +0530

    Update Q13.java

--- Assignment10/Q13.java
+++ Assignment10/Q13.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
-
 public class Q13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
         System.out.print("Enter the number of columns: ");
         int cols = scanner.nextInt();
-
         int[][] array = new int[rows][cols];
-
         System.out.println("Enter the elements of the " + rows + "x" + cols + " matrix:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 array[i][j] = scanner.nextInt();
             }
         }
-
         int min = array[0][0];
         int max = array[0][0];
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (array[i][j] < min) {
@@ -31,10 +25,8 @@ public class Q13 {
                 }
             }
         }
-
         System.out.println("Minimum value in the matrix: " + min);
         System.out.println("Maximum value in the matrix: " + max);
-
         scanner.close();
     }
 }
