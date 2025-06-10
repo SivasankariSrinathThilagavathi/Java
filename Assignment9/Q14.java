604ee92f031a56ad6f646f72844268e90f510fd7
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:27:24 +0530

    Update Q14.java

--- Assignment9/Q14.java
+++ Assignment9/Q14.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
-
 public class Q14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
         int[] numbers = new int[n];
-
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
-
         int sum = 0;
         for (int num : numbers) {
             if (isPrime(num)) {
                 sum += num;
             }
         }
-
         System.out.println("Sum of prime numbers: " + sum);
         scanner.close();
     }
-
     private static boolean isPrime(int num) {
         if (num < 2) {
             return false;
