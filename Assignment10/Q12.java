59ad5ea925cdbdc73cb8ef0e62d029a28fe12ab4
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:22:39 +0530

    Update Q12.java

--- Assignment10/Q12.java
+++ Assignment10/Q12.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
 public class Q12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
         System.out.print("Enter the number of columns: ");
         int cols = scanner.nextInt();
-
         int[][] matrix = new int[rows][cols];
-
         System.out.println("Enter the elements of the " + rows + "x" + cols + " matrix:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 matrix[i][j] = scanner.nextInt();
             }
         }
-
         for (int i = 0; i < rows; i++) {
             Arrays.sort(matrix[i]); // Sorting each row
         }
-
         System.out.println("Matrix after row-wise sorting:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -30,7 +24,6 @@ public class Q12 {
             }
             System.out.println();
         }
-
         scanner.close();
     }
 }
