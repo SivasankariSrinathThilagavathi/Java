ca939d4045a0c414d6a90aeb961478c66a0642eb
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:29:17 +0530

    Update Q3.java

--- Assignment9/Q3.java
+++ Assignment9/Q3.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
-
 public class Q3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[6];
-
-        // Accept 6 integers from the user
+        int[] numbers = new int[6];        
         System.out.println("Enter 6 numbers:");
         for (int i = 0; i < 6; i++) {
             numbers[i] = scanner.nextInt();
         }
-
-        // Initialize min and max variables
         int min = numbers[0];
-        int max = numbers[0];
-
-        // Find min and max values
+        int max = numbers[0];        
         for (int num : numbers) {
             if (num < min) {
                 min = num;
@@ -23,12 +16,9 @@ public class Q3 {
             if (num > max) {
                 max = num;
             }
-        }
-
-        // Display the results
+        }        
         System.out.println("Minimum value: " + min);
         System.out.println("Maximum value: " + max);
-
         scanner.close();
     }
 }
