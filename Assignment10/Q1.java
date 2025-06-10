aa6c07c54db5e8bf49f3e4bec5c52ff5a509e4dc
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:05:35 +0530

    Update Q1.java

--- Assignment10/Q1.java
+++ Assignment10/Q1.java
@@ -5,7 +5,6 @@ public class Q1 {
         Scanner scanner = new Scanner(System.in);
         int[][] matrix = new int[3][3];
 
-        // Accept user input for the 3x3 matrix
         System.out.println("Enter 9 numbers to fill the 3x3 matrix:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -13,13 +12,12 @@ public class Q1 {
             }
         }
 
-        // Print the matrix
         System.out.println("The 3x3 matrix is:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
-            System.out.println(); // Move to the next row
+            System.out.println(); 
         }
 
         scanner.close();
