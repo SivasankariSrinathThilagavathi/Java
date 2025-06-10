1643c59c22a502598282d81f06a3430d6030335a
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:21:50 +0530

    Update Q15.java

--- Assignment10/Q15.java
+++ Assignment10/Q15.java
@@ -1,23 +1,16 @@
 import java.util.Scanner;
-
 public class Q15 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Accept the number of rows and columns
+        Scanner scanner = new Scanner(System.in);        
         System.out.print("Enter the number of rows and columns (square matrix): ");
         int size = scanner.nextInt();
-        int[][] matrix = new int[size][size];
-
-        // Accept elements for the matrix
+        int[][] matrix = new int[size][size];        
         System.out.println("Enter the elements of the " + size + "x" + size + " matrix:");
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 matrix[i][j] = scanner.nextInt();
             }
-        }
-
-        // Check if the matrix is symmetric
+        }       
         boolean isSymmetric = true;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -27,15 +20,12 @@ public class Q15 {
                 }
             }
             if (!isSymmetric) break;
-        }
-
-        // Display the result
+        }        
         if (isSymmetric) {
             System.out.println("The matrix is symmetric.");
         } else {
             System.out.println("The matrix is not symmetric.");
         }
-
         scanner.close();
     }
 }
