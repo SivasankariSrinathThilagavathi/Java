7dbb5fe672f501c7f2b44459b5064ff640f39a16
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:24:16 +0530

    Update Q1.java

--- Assignment9/Q1.java
+++ Assignment9/Q1.java
@@ -1,22 +1,16 @@
 import java.util.Scanner;
-
 public class Q1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[10];
-
-        // Accept 10 integers from the user
         System.out.println("Enter 10 integers:");
         for (int i = 0; i < 10; i++) {
             numbers[i] = scanner.nextInt();
-        }
-
-        // Display the elements of the array
+        }      
         System.out.println("The entered numbers are:");
         for (int num : numbers) {
             System.out.print(num + " ");
         }
-
         scanner.close();
     }
 }
