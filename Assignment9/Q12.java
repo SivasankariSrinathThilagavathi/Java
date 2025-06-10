8c3f6578ce9ef3a3ead925230a544778fd9d11d6
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:26:37 +0530

    Update Q12.java

--- Assignment9/Q12.java
+++ Assignment9/Q12.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
 import java.util.LinkedHashSet;
-
 public class Q12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
         int[] numbers = new int[n];
-
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
-
         LinkedHashSet<Integer> uniqueElements = new LinkedHashSet<>();
         for (int num : numbers) {
             uniqueElements.add(num);
         }
-
         System.out.println("Array after removing duplicates:");
         for (int num : uniqueElements) {
             System.out.print(num + " ");
         }
-
         scanner.close();
     }
 }
