c64c52521b85c10590c4403b9ab6d6a9487441ad
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:29:47 +0530

    Update Q4.java

--- Assignment9/Q4.java
+++ Assignment9/Q4.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
-
 public class Q4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Accept the size of the array
+        Scanner scanner = new Scanner(System.in);        
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
-        int[] numbers = new int[n];
-
-        // Accept `n` integers
+        int[] numbers = new int[n];        
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
-        }
-
-        // Count even and odd numbers
+        }        
         int evenCount = 0, oddCount = 0;
         for (int num : numbers) {
             if (num % 2 == 0) {
@@ -24,11 +17,8 @@ public class Q4 {
                 oddCount++;
             }
         }
-
-        // Display the counts
         System.out.println("Even numbers count: " + evenCount);
         System.out.println("Odd numbers count: " + oddCount);
-
         scanner.close();
     }
 }
