3927116630f5283f8545d3630a9a61f0c1942f18
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-10 23:45:44 +0530

    Create Q14.java

---
+++ Assignment9/Q14.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int num : numbers) {
+            if (isPrime(num)) {
+                sum += num;
+            }
+        }
+
+        System.out.println("Sum of prime numbers: " + sum);
+        scanner.close();
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
