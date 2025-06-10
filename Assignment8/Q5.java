f76c889b306a0c56c551609790c2f20cac430c1f
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-10 23:33:40 +0530

    Create Q5.java

---
+++ Assignment8/Q5.java
@@ -0,0 +1,21 @@
+class BankAccount {
+    final void generateAccountNumber() {
+        System.out.println("Generating unique account number...");
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    // Attempting to override final method (will cause a compilation error)
+    /*
+    void generateAccountNumber() {
+        System.out.println("Generating savings account number...");
+    }
+    */
+}
+
+public class Q5 {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.generateAccountNumber();
+    }
+}
