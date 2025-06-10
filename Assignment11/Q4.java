cc69da677275aaed95793f2445fe57673fb438ec
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:46:41 +0530

    Create Q4.java

---
+++ Assignment11/Q4.java
@@ -0,0 +1,7 @@
+public class Q4 {
+    public static void main(String[] args) {        
+        char[] charArray = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+        String str = new String(charArray);
+        System.out.println("The new String is: " + str);
+    }
+}
