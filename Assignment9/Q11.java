0cf2de562e271d006a5c306ef3c5867485b43deb
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-10 23:44:01 +0530

    Create Q11.java

---
+++ Assignment9/Q11.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Q11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Accept the size of the array
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+
+        // Accept `n` elements from the user
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        // Find the second largest element
+        if (n < 2) {
+            System.out.println("At least two elements are required!");
+        } else {
+            int largest = Integer.MIN_VALUE;
+            int secondLargest = Integer.MIN_VALUE;
+
+            for (int num : numbers) {
+                if (num > largest) {
+                    secondLargest = largest;
+                    largest = num;
+                } else if (num > secondLargest && num != largest) {
+                    secondLargest = num;
+                }
+            }
+
+            if (secondLargest == Integer.MIN_VALUE) {
+                System.out.println("No second largest element found (all elements are the same).");
+            } else {
+                System.out.println("Second largest element: " + secondLargest);
+            }
+        }
+
+        scanner.close();
+    }
+}
