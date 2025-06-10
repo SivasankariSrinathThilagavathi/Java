348e2449b61c5243e1e5ad5f0cfe261963b4eb7c
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:27:01 +0530

    Update Q13.java

--- Assignment9/Q13.java
+++ Assignment9/Q13.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
-
 public class Q13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
         int[] numbers = new int[n];
-
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
-
         System.out.println("Palindrome numbers:");
         for (int num : numbers) {
             if (isPalindrome(num)) {
                 System.out.print(num + " ");
             }
         }
-
         scanner.close();
     }
-
     public static boolean isPalindrome(int num) {
         int original = num, reverse = 0;
         while (num > 0) {
