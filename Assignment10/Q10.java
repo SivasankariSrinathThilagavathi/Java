bf7475feb9a2710d370115d15330e1a0d0f0899f
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:06:03 +0530

    Update Q10.java

--- Assignment10/Q10.java
+++ Assignment10/Q10.java
@@ -4,12 +4,10 @@ public class Q10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Accept the number of rows and columns
         System.out.print("Enter the number of rows and columns (square matrix): ");
         int size = scanner.nextInt();
         int[][] matrix = new int[size][size];
 
-        // Accept elements for the matrix
         System.out.println("Enter the elements of the " + size + "x" + size + " matrix:");
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -17,7 +15,6 @@ public class Q10 {
             }
         }
 
-        // Print the main diagonal elements
         System.out.println("Main diagonal elements:");
         for (int i = 0; i < size; i++) {
             System.out.print(matrix[i][i] + " ");
