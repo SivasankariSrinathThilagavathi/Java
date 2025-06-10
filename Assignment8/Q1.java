b0c5b4500b8784566f4a157bdc6347f7a560f59e
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-10 23:31:01 +0530

    Create Q1.java

---
+++ Assignment8/Q1.java
@@ -0,0 +1,36 @@
+abstract class Animal {
+    abstract void makeSound();
+
+    void breathe() {
+        System.out.println("Breathing...");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks: Woof woof!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows: Meow meow!");
+    }
+}
+
+public class Q1{
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        System.out.println("Demonstrating Dog:");
+        dog.breathe();
+        dog.makeSound();
+
+        System.out.println("\nDemonstrating Cat:");
+        cat.breathe();
+        cat.makeSound();
+    }
+}
