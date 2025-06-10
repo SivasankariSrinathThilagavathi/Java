9385e29bcd7cf4721c3e0f9d0c81b6c06d480646
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-10 23:50:50 +0530

    Create Q6.java

---
+++ Assignment10/Q6.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix1 = new int[3][3];
+        int[][] matrix2 = new int[3][3];
+        int[][] differenceMatrix = new int[3][3];
+
+        // Accept first 3x3 matrix from the user
+        System.out.println("Enter the elements of the first 3x3 matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Accept second 3x3 matrix from the user
+        System.out.println("Enter the elements of the second 3x3 matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Perform matrix subtraction
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                differenceMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+            }
+        }
+
+        // Display the resulting difference matrix
+        System.out.println("Difference of the two matrices:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(differenceMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
