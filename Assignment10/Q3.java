d40b9b924eea4e43e93071e12e9ec191225dab33
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:20:49 +0530

    Update Q3.java

--- Assignment10/Q3.java
+++ Assignment10/Q3.java
@@ -1,26 +1,18 @@
 import java.util.Scanner;
-
 public class Q3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Accept the number of rows and columns
+        Scanner scanner = new Scanner(System.in);        
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
         System.out.print("Enter the number of columns: ");
         int cols = scanner.nextInt();
-
         int[][] array = new int[rows][cols];
-
-        // Accept elements for the 2D array
         System.out.println("Enter the elements of the " + rows + "x" + cols + " matrix:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 array[i][j] = scanner.nextInt();
             }
         }
-
-        // Compute row-wise sum
         System.out.println("Row-wise sum:");
         for (int i = 0; i < rows; i++) {
             int rowSum = 0;
@@ -29,7 +21,6 @@ public class Q3 {
             }
             System.out.println("Sum of row " + (i + 1) + ": " + rowSum);
         }
-
         scanner.close();
     }
 }
