5d959d43979eefd700183a12625bf2d6e1fc9cc7
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-11 00:19:06 +0530

    Update Q5.java

--- Assignment10/Q5.java
+++ Assignment10/Q5.java
@@ -1,36 +1,27 @@
 import java.util.Scanner;
-
 public class Q5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[][] matrix1 = new int[3][3];
         int[][] matrix2 = new int[3][3];
-        int[][] sumMatrix = new int[3][3];
-
-        // Accept first 3x3 matrix from the user
+        int[][] sumMatrix = new int[3][3];        
         System.out.println("Enter the elements of the first 3x3 matrix:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matrix1[i][j] = scanner.nextInt();
             }
-        }
-
-        // Accept second 3x3 matrix from the user
+        }        
         System.out.println("Enter the elements of the second 3x3 matrix:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matrix2[i][j] = scanner.nextInt();
             }
-        }
-
-        // Perform matrix addition
+        }        
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
-
-        // Display the resulting sum matrix
         System.out.println("Sum of the two matrices:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -38,7 +29,6 @@ public class Q5 {
             }
             System.out.println();
         }
-
         scanner.close();
     }
 }
