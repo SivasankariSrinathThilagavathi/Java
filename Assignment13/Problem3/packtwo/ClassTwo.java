155b2d7692cd83b3f5b8c7b1fff906770511749f
Sivasankari Srinath Thilagavathi <168209544+SivasankariSrinathThilagavathi@users.noreply.github.com>
2025-06-30 22:29:22 +0530

    Update ClassTwo.java

--- Assignment13/Problem3/packtwo/ClassTwo.java
+++ Assignment13/Problem3/packtwo/ClassTwo.java
@@ -8,10 +8,6 @@ public class ClassTwo {
 
         System.out.println("Accessing from packtwo.ClassTwo:");
 
-        System.out.println("publicVar = " + obj.publicVar); // ✅ Accessible
-
-        // System.out.println("privateVar = " + obj.privateVar); // ❌ Compile error
-        // System.out.println("protectedVar = " + obj.protectedVar); // ❌ Compile error
-        // System.out.println("defaultVar = " + obj.defaultVar); // ❌ Compile error
+        System.out.println("publicVar = " + obj.publicVar); 
     }
 }
